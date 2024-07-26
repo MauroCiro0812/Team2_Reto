51cfa1c65b5231ae07436980f7e0e00791eb28e5
Santifloroja18 <sflorez731@misena.edu.co>
2024-07-26 10:54:03 -0500

    V01.6

--- app.js
+++ app.js
@@ -20,3 +20,10 @@ loginButton.addEventListener('click', () => {
   userForms.classList.remove('bounceLeft')
   userForms.classList.add('bounceRight')
 }, false)
+
+getDocument.getElementById('cambiarfondo').addEventListener('click',
+  function(){
+    var imagen = document.getElementById("fondo-imagen");
+    imagen.style.backgroundImage = 'url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.vecteezy.com%2Ffotos-gratis%2Fplaya&psig=AOvVaw0VNq08GmfkehvspvkCizPk&ust=1722093839080000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNiowM2BxYcDFQAAAAAdAAAAABAE")';
+  }
+)
